3bd47f411d0e69522fc68274ac64ac0b2f86e21f
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2022-09-29 17:02:55 -0500

    DELETE-LOGIN-FILES
    Se eliminaron problemas de fusión de ramas.

--- SistemaConfiguracion/SistemaConfiguracionUrna/SCU.UWP/Views/Login/LoginPage.xaml.cs
+++ SistemaConfiguracion/SistemaConfiguracionUrna/SCU.UWP/Views/Login/LoginPage.xaml.cs
@@ -1,24 +1,8 @@
-<<<<<<< Updated upstream
-﻿using SCU.UWP.Models;
-using SCU.UWP.Views.Inicio;
-=======
 ﻿using SCU.UWP.Views.Dashboard;
->>>>>>> Stashed changes
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
