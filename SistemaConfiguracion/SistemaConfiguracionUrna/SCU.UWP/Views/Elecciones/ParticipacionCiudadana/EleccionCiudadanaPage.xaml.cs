7b0ca5fac5c8938eccddcd0bbf76e107ac56c313
DenisseHC21 <108244394+DenisseHC21@users.noreply.github.com>
2022-10-05 17:51:30 -0500

    JDHC_PAGE ELECCION CIUDADANA
    Se agrego la pagina de mecanismos de participación ciudadana dentro del apartado de elecciones.

--- SistemaConfiguracion/SistemaConfiguracionUrna/SCU.UWP/Views/Elecciones/ParticipacionCiudadana/EleccionCiudadanaPage.xaml.cs
+++ SistemaConfiguracion/SistemaConfiguracionUrna/SCU.UWP/Views/Elecciones/ParticipacionCiudadana/EleccionCiudadanaPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using SCU.UWP.Views.Elecciones.ParticipacionCiudadana;
+﻿using SCU.UWP.Views.Partidos;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Input.Inking;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,32 +16,34 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace SCU.UWP.Views.Elecciones
+namespace SCU.UWP.Views.Elecciones.ParticipacionCiudadana
 {
     /// <summary>
     /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class EleccionesPage : Page
+    public sealed partial class EleccionCiudadanaPage : Page
     {
-        public EleccionesPage()
+
+        int txtbox = 1;
+        public EleccionCiudadanaPage()
         {
             this.InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
         {
-            NuevaEleccionPage mynewPage = new NuevaEleccionPage();
+            EleccionesPage mynewPage = new EleccionesPage();
             this.Content = mynewPage;
         }
-
-        private void Button_Participacion_Ciudadana(object sender, RoutedEventArgs e)
+        private async void btnNuevoPartido_Click(object sender, RoutedEventArgs e)
         {
-            EleccionCiudadanaPage mynewPage = new EleccionCiudadanaPage();
-            this.Content = mynewPage;
+            AgregarPartido agregarPartido = new AgregarPartido();
+            await agregarPartido.ShowAsync();
         }
+
 
     }
 }
