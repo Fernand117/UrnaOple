7656f74444fafe3b1ecfc7455b8f5831eda1e4ed
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2022-10-12 10:06:50 -0500

    LFLL-UPDATE-ELECCIONES
    Se cambio el localhost

--- SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.DAL/Context/ConfiContext.cs
+++ SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.DAL/Context/ConfiContext.cs
@@ -6,7 +6,7 @@ namespace CONFIG.DAL.Context
     {
         public HttpWebRequest configuracionApi(string method)
         {
-            var url = $"http://localhost:32042/api/eleccion";
+            var url = $"http://localhost:5000/api/eleccion";
             var request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = method;
             request.ContentType = "application/json";
