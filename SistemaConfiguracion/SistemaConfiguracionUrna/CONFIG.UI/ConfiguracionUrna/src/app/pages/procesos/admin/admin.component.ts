3734ac68b9a1688a7ef38a93773b9c3c6ba918ad
Fernand117 <fernando_rendel892@hotmail.com>
2022-10-12 16:27:15 -0500

    LFLL-ELECCIONES
    Se diseñó el modal para añadir partidos

--- SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/pages/procesos/admin/admin.component.ts
+++ SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/pages/procesos/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  public imagePath: any;
+  imgURL: any;
+  public message: string;
+  //myForm: FormGroup;
+
+  constructor(
+  ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  addPartido() {
+    var modal = document.getElementById("formPartidos");
+    modal!.style.display = "block";
+  }
+
+  closeModal() {
+    var modal = document.getElementById("formPartidos");
+    modal!.style.display = "none";
+  }
+
+  preview(files: any) {
+    if (files.length === 0)
+      return;
+
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+    }
   }
 
 }
