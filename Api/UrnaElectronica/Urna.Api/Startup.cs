b0bd2fbb152d9cc4984a2cfac7a5dd2241ee1666
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2022-10-17 12:59:29 -0500

    LFLL-UPDATE-CORS
    Se estableció la configuración que permite conectar con el API mediante http desde cualquier dirección host

--- Api/UrnaElectronica/Urna.Api/Startup.cs
+++ Api/UrnaElectronica/Urna.Api/Startup.cs
@@ -51,12 +51,11 @@ namespace Urna.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(options =>
-            {
-                options.WithOrigins("http://localhost:4200");
-                options.AllowAnyMethod();
-                options.AllowAnyHeader();
-            });
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+            );
 
             if (env.IsDevelopment())
             {
