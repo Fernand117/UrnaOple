20e254c14ab2512cc5b8bf14f51c3cf48e16226b
OscarGalvxn <64057422+OscarGalvxn@users.noreply.github.com>
2022-10-22 15:09:22 -0500

    ORGC-UPDATE-ESCOLARES
    Implementación de Elecciones Escolares terminado

---
+++ Api/UrnaElectronica/Urna.Api/Controllers/Escolar/EscolarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Urna.BLO.Escolares;
+using Urna.COMMON.DTOS;
+using Urna.COMMON.DTOS.Escolares;
+
+namespace Urna.Api.Controllers.Escolar
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EscolarController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> Get() => await new EscolarBLO().Read();
+
+        [HttpGet("code/{codigo}")]
+        public async Task<ActionResult<ApiResponse>> Get(string codigo) => await new EscolarBLO().Read(codigo);
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> Post([FromBody] EscolarRequest request) => await new EscolarBLO().Create(request);
+
+        [HttpPut]
+        public async Task<ActionResult<ApiResponse>> Put([FromBody] EscolarRequest request) => await new EscolarBLO().Update(request);
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ApiResponse>> Delete(int id) => await new EscolarBLO().Delete(id);
+    }
+}
---
+++ Api/UrnaElectronica/Urna.DAL/Migrations/20221022190057_inicial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Urna.DAL.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "configuracion",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Codigo = table.Column<string>(nullable: true),
+                    Categoria = table.Column<string>(nullable: true),
+                    configuracion = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbConfiguracion", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "configuracion");
+        }
+    }
+}
