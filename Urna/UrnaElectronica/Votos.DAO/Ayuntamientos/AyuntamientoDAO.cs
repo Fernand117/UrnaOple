bc467d6729b059994b019fa55209fc21feeacb9e
OscarGalvxn <64057422+OscarGalvxn@users.noreply.github.com>
2022-10-23 01:06:08 -0500

    ORGC-CREATE-API_VOTOS
    Creacion del proyecto WebApi para el proceso de votaciones.
    Capas creadas, conexión a base de datos creada.
    Proyecto conectado.

---
+++ Urna/UrnaElectronica/Votos.DAO/Ayuntamientos/AyuntamientoDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Votos.COMMON.DTOS.Ayuntamientos;
+using Votos.DAL.Context;
+using Votos.DAL.Entities.Ayuntamientos;
+
+namespace Votos.DAO.Ayuntamientos
+{
+    public class AyuntamientoDAO
+    {
+        public async Task<AyuntamientoRequest> Create(AyuntamientoRequest request)
+        {
+			try
+			{
+				using (VotoContext context = new VotoContext())
+				{
+					Ayuntamiento ayuntamiento = new Ayuntamiento()
+					{
+						Id = request.Id,
+						Partido = request.Partido,
+						Voto = request.Voto
+					};
+
+					await context.AddAsync(ayuntamiento);
+					await context.SaveChangesAsync();
+				}
+			}
+			catch (Exception) { }
+
+			return request;
+        }
+    }
+}
