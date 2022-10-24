b7b4da194cb8fa0a03280283c446cfbba7f218b6
Fernand117 <fernando_rendel892@hotmail.com>
2022-10-24 13:04:28 -0500

    LFLL-ESCOLARES
    Se programó la función que permite enviar la configuración de las elecciones escolares al servidor

--- SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/services/api-service.service.ts
+++ SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/services/api-service.service.ts
@@ -1,20 +1,21 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  private url = "http://localhost:32042/api/eleccion";
+  private url = "http://localhost:5000/api";
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+  }
 
   uploadSignature(vals: any) {
     let data = vals;
-    return this.http.post('https://api.cloudinary.com/v1_1/ddwh8eqlw/upload',data)
+    return this.http.post('https://api.cloudinary.com/v1_1/ddwh8eqlw/upload', data)
   }
 
   listaConfiguraciones() {
@@ -22,10 +23,14 @@ export class ApiServiceService {
   }
 
   guardarConfiguracion(datos: any) {
-    return this.http.post(`${this.url}/eleccion`, datos, {headers : new HttpHeaders({ 'Content-Type': 'application/json' })});
+    return this.http.post(`${this.url}/eleccion`, datos, {headers: new HttpHeaders({'Content-Type': 'application/json'})});
   }
 
   guardarMecanismo(datos: any) {
-    return this.http.post(`${this.url}/mecanismo`, datos, {headers : new HttpHeaders({ 'Content-Type': 'application/json' })});
+    return this.http.post(`${this.url}/mecanismo`, datos, {headers: new HttpHeaders({'Content-Type': 'application/json'})});
+  }
+
+  guardarEscolares(datos: any) {
+    return this.http.post(`${this.url}/escolar`, datos, {headers: new HttpHeaders({'Content-Type': 'application/json'})});
   }
 }
