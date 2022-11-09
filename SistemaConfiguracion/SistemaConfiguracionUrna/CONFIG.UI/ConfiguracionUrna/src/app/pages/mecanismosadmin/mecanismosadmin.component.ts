40ae06f742c482378edbede91cef2f083fd54c12
Fernand117 <fernando_rendel892@hotmail.com>
2022-11-09 12:09:11 -0600

    LFLL-UPDATE-MECANISMOS
    Se añadieron las funciones para poder editar y eliminar registros de los mecanismos

--- SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/pages/mecanismosadmin/mecanismosadmin.component.ts
+++ SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/pages/mecanismosadmin/mecanismosadmin.component.ts
@@ -13,14 +13,27 @@ import Swal from 'sweetalert2';
 export class MecanismosadminComponent implements OnInit {
 
   public mecanismos: MecanismosModule = new MecanismosModule();
+  public mecanismosUpdate: MecanismosModule = new MecanismosModule();
+
+  public preguntaUpdate: PreguntasModule = new PreguntasModule();
   public pregunta: PreguntasModule;
 
   public listaPreguntas: any[] = [];
   public listaMecanismos: any[] = [];
 
+  public listaPreguntasUpdate: any[] = [];
+  public listaMecanismosUpdate: any[] = [];
+
   private resData: any;
 
   private contador: number;
+  private modoEdiccionPregunta: boolean;
+
+  private consultaMecanismo: any;
+  private resConsultaMecanismo: any;
+
+  private consultaPregunta: any;
+  private resConsultaPregunta: any;
 
   constructor(
     private apiService: ApiServiceService,
@@ -29,6 +42,7 @@ export class MecanismosadminComponent implements OnInit {
 
   ngOnInit(): void {
     this.contador = 0;
+    this.modoEdiccionPregunta = false;
   }
 
   addPartido() {
@@ -42,7 +56,32 @@ export class MecanismosadminComponent implements OnInit {
     modal!.style.display = "none";
   }
 
+  openModalEditMecanismo() {
+    var modal = document.getElementById("formPartidosUpdate");
+    modal!.style.display = "block";
+  }
+
+  closeModalEditMecanismos() {
+    var modal = document.getElementById("formPartidosUpdate");
+    modal!.style.display = "none";
+  }
+
+  openModalEditPregunta() {
+    var modal = document.getElementById("formPartidosU");
+    modal!.style.display = "block";
+    modal!.style.zIndex = "100";
+  }
+
+  closeModalEditPregunta() {
+    var modal = document.getElementById("formPartidosU");
+    modal!.style.display = "none";
+  }
+
   agregarPregunta() {
+    if (this.pregunta.pregunta === "") {
+      this.mensajeError("Debe registrar una pregunta.");
+      return;
+    }
     this.listaPreguntas.push(this.pregunta);
     this.closeModal();
   }
@@ -122,6 +161,11 @@ export class MecanismosadminComponent implements OnInit {
       return;
     }
 
+    if (this.listaPreguntas.length === 0) {
+      this.mensajeError("Aún no añadido preguntas al mecanismo que desea configurar.");
+      return;
+    }
+
     const datos = {
       "TipoMecanismo": this.mecanismos.TipoMecanismo,
       "Nombre": this.mecanismos.Nombre,
@@ -179,5 +223,100 @@ export class MecanismosadminComponent implements OnInit {
         this.router.navigateByUrl('/elecciones')
       }
     );
+  }
+
+  public editarConfiguracion(nombre: string) {
+    this.consultaMecanismo = this.listaMecanismos.findIndex(m => m.Nombre === nombre);
+    this.resConsultaMecanismo = this.listaMecanismos[this.consultaMecanismo];
+
+    console.log(this.consultaMecanismo)
+    console.log(this.resConsultaMecanismo)
+
+    this.mecanismosUpdate.TipoMecanismo = this.resConsultaMecanismo["TipoMecanismo"];
+    this.mecanismosUpdate.Nombre = this.resConsultaMecanismo["Nombre"];
+    this.mecanismosUpdate.Objeto = this.resConsultaMecanismo["Objeto"];
+    this.mecanismosUpdate.Presidente = this.resConsultaMecanismo["Presidente"];
+    this.mecanismosUpdate.Secretario = this.resConsultaMecanismo["Secretario"];
+    this.mecanismosUpdate.PrimerEscrutador = this.resConsultaMecanismo["PrimerEscrutador"];
+    this.mecanismosUpdate.SegundoEscrutador = this.resConsultaMecanismo["SegundoEscrutador"];
+    this.mecanismosUpdate.Entidad = this.resConsultaMecanismo["Entidad"];
+    this.mecanismosUpdate.Distrito = this.resConsultaMecanismo["Distrito"];
+    this.mecanismosUpdate.Municipio = this.resConsultaMecanismo["Municipio"];
+    this.mecanismosUpdate.Seccion = this.resConsultaMecanismo["SeccionElectoral"];
+    this.mecanismosUpdate.Folio = this.resConsultaMecanismo["Folio"];
+    this.mecanismosUpdate.CantidadBoletas = this.resConsultaMecanismo["CantidadBoletas"];
+    this.listaPreguntasUpdate = this.resConsultaMecanismo["Preguntas"];
+    console.log(this.mecanismosUpdate)
+
+    this.openModalEditMecanismo();
+  }
+
+  public eliminarConfiguracion(nombre: string) {
+    this.consultaMecanismo = this.listaMecanismos.findIndex(m => m.Nombre === nombre);
+    this.resConsultaMecanismo = this.listaMecanismos[this.consultaMecanismo];
+    this.listaMecanismos.splice(this.listaMecanismos.indexOf(this.resConsultaPregunta), 1);
+  }
+
+  public guardarConfiguracionActualizada() {
+    this.listaMecanismos[this.consultaMecanismo].TipoMecanismo = this.mecanismosUpdate.TipoMecanismo;
+    this.listaMecanismos[this.consultaMecanismo].Nombre = this.mecanismosUpdate.Nombre;
+    this.listaMecanismos[this.consultaMecanismo].Objeto = this.mecanismosUpdate.Objeto;
+    this.listaMecanismos[this.consultaMecanismo].Presidente = this.mecanismosUpdate.Presidente;
+    this.listaMecanismos[this.consultaMecanismo].Secretario = this.mecanismosUpdate.Secretario;
+    this.listaMecanismos[this.consultaMecanismo].PrimerEscrutador = this.mecanismosUpdate.PrimerEscrutador;
+    this.listaMecanismos[this.consultaMecanismo].SegundoEscrutador = this.mecanismosUpdate.SegundoEscrutador;
+    this.listaMecanismos[this.consultaMecanismo].Entidad = this.mecanismosUpdate.Entidad;
+    this.listaMecanismos[this.consultaMecanismo].Distrito = this.mecanismosUpdate.Distrito;
+    this.listaMecanismos[this.consultaMecanismo].Municipio = this.mecanismosUpdate.Municipio;
+    this.listaMecanismos[this.consultaMecanismo].Seccion = this.mecanismosUpdate.Seccion;
+    this.listaMecanismos[this.consultaMecanismo].Folio = this.mecanismosUpdate.Folio;
+    this.listaMecanismos[this.consultaMecanismo].CantidadBoletas = this.mecanismosUpdate.CantidadBoletas;
+    this.listaMecanismos[this.consultaMecanismo].Preguntas = this.listaPreguntasUpdate;
+
+    console.log(this.listaMecanismos);
+
+    this.closeModalEditMecanismos();
+  }
+
+  public editarPreguntaActual(pregunta: string) {
+    this.consultaPregunta = this.listaPreguntas.findIndex(p => p.pregunta === pregunta);
+    this.resConsultaPregunta = this.listaPreguntas[this.consultaPregunta];
+    console.log(this.resConsultaPregunta);
+    this.preguntaUpdate.pregunta = this.resConsultaPregunta["pregunta"];
+
+    this.openModalEditPregunta();
+  }
+
+  public eliminarPreguntaActual(pregunta: string) {
+    this.consultaPregunta = this.listaPreguntas.findIndex(p => p.pregunta === pregunta);
+    this.resConsultaPregunta = this.listaPreguntas[this.consultaPregunta];
+    console.log(this.resConsultaPregunta);
+    this.listaPreguntas.splice(this.listaPreguntas.indexOf(this.resConsultaPregunta), 1);
+  }
+
+  public guardarPreguntaActual() {
+    if (this.modoEdiccionPregunta === true) {
+      this.listaPreguntasUpdate[this.consultaPregunta].pregunta = this.preguntaUpdate.pregunta;
+      this.closeModalEditPregunta();
+      return;
+    }
+    this.listaPreguntas[this.consultaPregunta].pregunta = this.preguntaUpdate.pregunta;
+    this.closeModalEditPregunta();
+  }
+
+  public editarPreguntaUpdate(pregunta: string) {
+    this.modoEdiccionPregunta = true;
+    this.consultaPregunta = this.listaPreguntasUpdate.findIndex(p => p.pregunta === pregunta);
+    this.resConsultaPregunta = this.listaPreguntasUpdate[this.consultaPregunta];
+
+    this.preguntaUpdate.pregunta = this.resConsultaPregunta["pregunta"];
+    this.openModalEditPregunta();
+  }
+
+  public eliminarPreguntaUpdate(pregunta: string) {
+    this.consultaPregunta = this.listaPreguntasUpdate.findIndex(p => p.pregunta === pregunta);
+    this.resConsultaPregunta = this.listaPreguntasUpdate[this.consultaPregunta];
+
+    this.listaPreguntasUpdate.splice(this.listaPreguntasUpdate.indexOf(this.resConsultaPregunta), 1);
   }
 }
