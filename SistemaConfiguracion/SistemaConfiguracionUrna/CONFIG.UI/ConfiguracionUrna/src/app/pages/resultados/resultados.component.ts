04a3eab5ae0161fe8980c7b8903d56c1916cf4a1
Fernand117 <fernando_rendel892@hotmail.com>
2022-11-20 15:26:02 -0600

    LFLL-UPDATE-RESULTADOS
    Se ha vacíado la db de los resultados en el servidor para poder publicar unos nuevos.
    
    Se han puesto las listas para mostrar los resultados de las votaciones dependiendo de la candidatura

--- SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/pages/resultados/resultados.component.ts
+++ SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/pages/resultados/resultados.component.ts
@@ -14,10 +14,15 @@ export class ResultadosComponent implements OnInit {
   public data: any[] = [];
   public resCategorias: any[] = [];
 
-  //VARIABLES PARA SACAR LAS CATEGORIAS
+  // VARIABLES PARA SACAR LAS CATEGORIAS
   public TIPO_ELECCION_UNO: string;
   public TIPO_ELECCION_DOS: string;
   public TIPO_ELECCION_TRES: string;
+
+  // LISTAS PARA GUARDAR LOS VOTOS POR CADA TIPO DE ELECCIÓN
+  public LISTA_VOTOS_GUBERNATURA: any[] = [];
+  public LISTA_VOTOS_DIPUTACIONES: any[] = [];
+  public LISTA_VOTOS_AYUNTAMIENTOS: any[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -32,32 +37,51 @@ export class ResultadosComponent implements OnInit {
         this.resServidor = res;
         this.resDatosServidor = this.resServidor;
         this.data = this.resDatosServidor['data'];
-        console.log(this.data)
         for (let i = 0; i < this.data.length; i++) {
-          //this.jsonParse = JSON.parse(this.data[i]['resultados']);
           this.resCategorias.push(JSON.parse(JSON.parse(this.data[i]['resultados'])));
         }
+
         console.log(this.resCategorias)
 
         for (let i = 0; i < this.resCategorias.length; i++) {
-          if (this.resCategorias[i]['categoria'] === "Elecciones escolares") {
-            this.TIPO_ELECCION_UNO = this.resCategorias[i]['categoria'];
-          }
           for (let j = 0; j < this.resCategorias[i].length; j++) {
-            console.log(this.resCategorias[i][j]['TipoEleccion']);
             if (this.resCategorias[i][j]['TipoEleccion'] === "Gubernatura") {
               this.TIPO_ELECCION_UNO = this.resCategorias[i][j]['TipoEleccion'];
+              this.LISTA_VOTOS_GUBERNATURA.push(this.resCategorias[i][j]['Datos']);
             }
 
             if (this.resCategorias[i][j]['TipoEleccion'] === "Diputacion") {
               this.TIPO_ELECCION_DOS = this.resCategorias[i][j]['TipoEleccion'];
+              this.LISTA_VOTOS_DIPUTACIONES.push(this.resCategorias[i][j]['Datos']);
             }
 
             if (this.resCategorias[i][j]['TipoEleccion'] === "Ayuntamiento") {
               this.TIPO_ELECCION_TRES = this.resCategorias[i][j]['TipoEleccion'];
+              this.LISTA_VOTOS_AYUNTAMIENTOS.push(this.resCategorias[i][j]['Datos']);
+            }
+
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Consulta popular") {
+              this.TIPO_ELECCION_UNO = this.resCategorias[i][j]['TipoEleccion'];
+            }
+
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Referendúm") {
+              this.TIPO_ELECCION_DOS = this.resCategorias[i][j]['TipoEleccion'];
+            }
+
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Plesbicito") {
+              this.TIPO_ELECCION_TRES = this.resCategorias[i][j]['TipoEleccion'];
+            }
+
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Elección escolar") {
+              this.TIPO_ELECCION_UNO = this.resCategorias[i][j]['TipoEleccion'];
             }
           }
         }
+
+        console.log(this.LISTA_VOTOS_GUBERNATURA);
+        console.log(this.LISTA_VOTOS_DIPUTACIONES);
+        console.log(this.LISTA_VOTOS_AYUNTAMIENTOS);
+
       }
     );
   }
