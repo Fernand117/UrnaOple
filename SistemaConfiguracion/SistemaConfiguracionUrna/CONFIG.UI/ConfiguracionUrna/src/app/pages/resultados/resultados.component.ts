45fe52e2a50ca9c9a9428894ed621a2de7b92291
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2022-11-20 20:34:23 -0600

    LFLL-UPDATE-RESULTADOS
    Se muestran ya los resultados de las votaciones por su respectiva categoría.

--- SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/pages/resultados/resultados.component.ts
+++ SistemaConfiguracion/SistemaConfiguracionUrna/CONFIG.UI/ConfiguracionUrna/src/app/pages/resultados/resultados.component.ts
@@ -43,37 +43,86 @@ export class ResultadosComponent implements OnInit {
 
         console.log(this.resCategorias)
 
+        if (this.resCategorias.length > 0) {
+          document.getElementById("nav-tabContent")!.style.display = "block";
+        }
+
         for (let i = 0; i < this.resCategorias.length; i++) {
           for (let j = 0; j < this.resCategorias[i].length; j++) {
-            if (this.resCategorias[i][j]['TipoEleccion'] === "Gubernatura") {
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Gubernaturas") {
               this.TIPO_ELECCION_UNO = this.resCategorias[i][j]['TipoEleccion'];
               this.LISTA_VOTOS_GUBERNATURA.push(this.resCategorias[i][j]['Datos']);
+
+              document.getElementById("tdPregunta1")!.style.display = "none";
+              document.getElementById("tdSi1")!.style.display = "none";
+              document.getElementById("tdNo1")!.style.display = "none";
+
+              document.getElementById("nav-home-tab")!.style.display = "block";
             }
 
-            if (this.resCategorias[i][j]['TipoEleccion'] === "Diputacion") {
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Diputaciones") {
               this.TIPO_ELECCION_DOS = this.resCategorias[i][j]['TipoEleccion'];
               this.LISTA_VOTOS_DIPUTACIONES.push(this.resCategorias[i][j]['Datos']);
+
+              document.getElementById("tdPregunta2")!.style.display = "none";
+              document.getElementById("tdSi2")!.style.display = "none";
+              document.getElementById("tdNo2")!.style.display = "none";
+
+              document.getElementById("nav-profile-tab")!.style.display = "block";
             }
 
-            if (this.resCategorias[i][j]['TipoEleccion'] === "Ayuntamiento") {
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Ayuntamientos") {
               this.TIPO_ELECCION_TRES = this.resCategorias[i][j]['TipoEleccion'];
               this.LISTA_VOTOS_AYUNTAMIENTOS.push(this.resCategorias[i][j]['Datos']);
+
+              document.getElementById("tdPregunta3")!.style.display = "none";
+              document.getElementById("tdSi3")!.style.display = "none";
+              document.getElementById("tdNo3")!.style.display = "none";
+
+              document.getElementById("nav-contact-tab")!.style.display = "block";
             }
 
-            if (this.resCategorias[i][j]['TipoEleccion'] === "Consulta popular") {
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Consulta") {
               this.TIPO_ELECCION_UNO = this.resCategorias[i][j]['TipoEleccion'];
+              this.LISTA_VOTOS_GUBERNATURA.push(this.resCategorias[i][j]['Datos']);
+
+              document.getElementById("tdCandidato1")!.style.display = "none";
+              document.getElementById("tdVotos1")!.style.display = "none";
+
+              document.getElementById("nav-home-tab")!.style.display = "block";
             }
 
-            if (this.resCategorias[i][j]['TipoEleccion'] === "Referendúm") {
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Referendum") {
               this.TIPO_ELECCION_DOS = this.resCategorias[i][j]['TipoEleccion'];
+              this.LISTA_VOTOS_DIPUTACIONES.push(this.resCategorias[i][j]['Datos']);
+
+              document.getElementById("tdCandidato2")!.style.display = "none";
+              document.getElementById("tdVotos2")!.style.display = "none";
+
+              document.getElementById("nav-profile-tab")!.style.display = "block";
             }
 
-            if (this.resCategorias[i][j]['TipoEleccion'] === "Plesbicito") {
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Presbicito") {
               this.TIPO_ELECCION_TRES = this.resCategorias[i][j]['TipoEleccion'];
+              this.LISTA_VOTOS_AYUNTAMIENTOS.push(this.resCategorias[i][j]['Datos']);
+
+              document.getElementById("tdCandidato3")!.style.display = "none";
+              document.getElementById("tdVotos3")!.style.display = "none";
+
+              document.getElementById("nav-contact-tab")!.style.display = "block";
             }
 
-            if (this.resCategorias[i][j]['TipoEleccion'] === "Elección escolar") {
+            if (this.resCategorias[i][j]['TipoEleccion'] === "Escolares") {
               this.TIPO_ELECCION_UNO = this.resCategorias[i][j]['TipoEleccion'];
+              this.LISTA_VOTOS_GUBERNATURA.push(this.resCategorias[i][j]['Datos']);
+
+              document.getElementById("tdPregunta1")!.style.display = "none";
+              document.getElementById("tdSi1")!.style.display = "none";
+              document.getElementById("tdNo1")!.style.display = "none";
+
+              document.getElementById("nav-home-tab")!.style.display = "block";
+              document.getElementById("nav-profile-tab")!.style.display = "none";
+              document.getElementById("nav-contact-tab")!.style.display = "none";
             }
           }
         }
